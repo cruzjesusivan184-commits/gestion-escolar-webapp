d424b96525c5d590f91b61810e17b1e8a7557d38
Luis Yael Mendez Sanchez <luisyaelms@gmail.com>
2026-04-29 15:17:50 -0600

    Login de usuarios
    Se realiza el login del user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,12 @@ export const routes: Routes = [
     path: 'registro-usuarios',
     loadComponent: () => import('./screens/registro-usuarios-screen/registro-usuarios-screen').then(m => m.RegistroUsuariosScreen),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./screens/home-screen/home-screen').then(m => m.HomeScreen),
+  },
+
+  // TODO:Agregar la forma correcta de rutas con el Auth-layout y el Dashboard-layout para cada tipo de usuario (administrador, maestro, alumno)
 
   { path: '**', redirectTo: 'login' },
 ];
